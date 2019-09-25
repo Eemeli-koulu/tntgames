7049c05aa84e818a8f5f38b25607c9d35db093d7
eemeli <eemeli.kiminki@edu.lapinamk.fi>
2019-09-25 18:49:00 +0300

    Events test

---
+++ tntgames/Assets/Scripts/Events/RaceStarts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RaceStarts : MonoBehaviour
+{
+    public UnityEvent start;
+
+    void OnCollisionEnter(Collision col)
+    {
+        start.Invoke();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
