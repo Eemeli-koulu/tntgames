fd891c81f64e7f58fbaa265dcefe875b2b63303a
eemeli <eemeli.kiminki@edu.lapinamk.fi>
2019-10-01 23:29:39 +0300

    Menu text animation

--- tntgames/Assets/Scripts/MainMenu.cs
+++ tntgames/Assets/Scripts/MainMenu.cs
@@ -15,4 +15,5 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Application quit");
         Application.Quit();
     }
+ 
 }
---
+++ tntgames/Assets/Scripts/SelectAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class SelectAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Animator animator = GetComponent<Animator>();
+        if(animator != null)
+        {
+            animator.Play("MenuButtonAnimation");
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit");
+
+    }
+
+}
