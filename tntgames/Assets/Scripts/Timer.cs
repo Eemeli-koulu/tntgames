423bfccb463b769c083e5cf29ffdcea573d31d6d
eemeli <eemeli.kiminki@edu.lapinamk.fi>
2019-10-02 22:00:13 +0300

    timer

---
+++ tntgames/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart;
+    public Text textBox;
+
+    bool timerActive = false;
+
+    void start()
+    {
+        textBox.text = timeStart.ToString("F2");
+
+    }
+    void Update()
+    {
+        if (timerActive)
+        {
+            timeStart += Time.deltaTime;
+            textBox.text = timeStart.ToString("F2");
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        timerActive = !timerActive;
+    }
+}
